5a03c95dc0f3512a5911a9056480b9204d7bb116
laurenlmylee <85115625+laurenlmylee@users.noreply.github.com>
2021-07-05 20:46:33 +0100

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-alert("Hello!");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = `3c3f4f4183259372b36704ba2802ee9d`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${apiKey}`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
