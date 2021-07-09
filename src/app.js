8c33fc026ae25bcace18a0b45ce6458629c038d6
laurenlmylee <85115625+laurenlmylee@users.noreply.github.com>
2021-07-09 14:20:09 +0100

    Started JS for weather forecast

--- src/app.js
+++ src/app.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+
+<div class="col-2">
+<div class="weather-forecast-date">${day}</div>
+<img src="https://openweathermap.org/img/wn/10d@2x.png" alt="" width="45" />
+<div class="weather-forecast" id="forecast">
+
+<div class="weather-forecast-temp">
+  <span class="weather-forecast-temp-max">18°
+      </span> 
+  <span class="weather-forecast-temp-min">14°</span>
+</div>
+</div>
+`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -86,3 +115,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
 
 search("London");
+displayForecast();
